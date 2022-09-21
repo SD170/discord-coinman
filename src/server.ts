113bc73725ca521824eb5f490d94433322297c56
SD170 <duttasaswata7@gmail.com>
2022-09-21 16:48:18 +0530

    bot login

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("server");
+import { Client, GatewayIntentBits } from "discord.js";
+import dotenv from "dotenv";
+
+
+//load env vars
+dotenv.config({path: __dirname+'/../.env'});
+
+
+// this allows us to recieve Guilds and GuildMessages events.
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+// we need the token to login the bot
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+// bot is live!!
+client.login(BOT_TOKEN);
+
