99122a1e3db7300ae2cb9fac3dd606fa6a11feaf
SD170 <duttasaswata7@gmail.com>
2022-09-25 12:30:47 +0530

    remote redis added

--- src/cache/redisConfig.ts
+++ src/cache/redisConfig.ts
@@ -5,19 +5,19 @@ import redis, { createClient, RedisClientType } from "redis";
 let redisClient: RedisClientType;
 
 const connectRedis = async () => {
-    const {REDIS_CONNECTION,REDIS_HOST,REDIS_PORT} = process.env;
-    const redisUrl = `${REDIS_CONNECTION}://${REDIS_HOST}:${REDIS_PORT}`; 
-    
+    const { REDIS_CONNECTION, REDIS_HOST, REDIS_PASSWORD, REDIS_CONTAINER_REG_DOM, REDIS_PORT } = process.env;
+    const redisUrl = `${REDIS_CONNECTION}://${REDIS_HOST}:${REDIS_PASSWORD}@${REDIS_CONTAINER_REG_DOM}:${REDIS_PORT}`;
+
     redisClient = createClient({
         url: redisUrl
     });
-    
+
     redisClient.on("error", (error) => {
         console.error(`Redis error: ${error}`)
         console.log(`redisUrl: ${redisUrl}`);
     });
-    
-    
+
+
     redisClient.on("connect", (error) => {
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
@@ -25,7 +25,7 @@ const connectRedis = async () => {
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
         console.log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
     });
-    
+
     await redisClient.connect();
 };
 
